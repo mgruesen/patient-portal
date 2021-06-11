43dd71a341ba9399a5262af10d53f6a020b16a6f
Michael Gruesen <mgruesen7@gmail.com>
2021-06-11 04:32:46 +0000

    Seed test users

--- PatientPortal.Database/init.sql
+++ PatientPortal.Database/init.sql
@@ -1,6 +1,7 @@
 USE [master];
 GO
 
+/* Add application database user */
 IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = N'app')
 BEGIN
     CREATE LOGIN app WITH PASSWORD = N'notsecure';
@@ -11,11 +12,15 @@ BEGIN
 END;
 GO
 
-INSERT INTO 
-    dbo.Patients (FirstName, LastName) 
-VALUES 
-    (N'Bob', N'Roberts'), 
-    (N'Bill', N'Williams');
+/* Add test users */
+IF NOT EXISTS (SELECT 1 FROM dbo.Users WHERE Username IN (N'bob', N'bill'))
+BEGIN
+    INSERT INTO 
+        dbo.Users (Id, Username, [Password]) 
+    VALUES 
+        -- bob:password1
+        ('41fb6be0-5827-4226-8c93-e9a96701644d', N'bob', N'10000.WqcaDoKgiNEloduj0hELNA==.P7r8Tq20te86gr8ZpwJRs5JmvMo2wiHxsTA95C3ZyqM='),
+        -- bill:password2
+        ('75bf4d2e-3410-4d79-a33b-1c63402a6af3', N'bill', N'10000.iLl3E45elWO0WEJp4EVc9Q==.3a/Bo6vx2NXweAodzPhLTSiFmJYInMRYXPYMA4eLYfE=');
+END;
 GO
-
-INSERT INTO dbo.Users (Username, [Password]) VALUES (N'bob', N'10000.salt.key');
