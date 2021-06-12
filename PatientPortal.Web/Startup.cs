8cce098809190220ce11247f9fb9d79f26a1f98c
Michael Gruesen <mgruesen7@gmail.com>
2021-06-12 00:42:28 +0000

    Add auth

--- PatientPortal.Web/Startup.cs
+++ PatientPortal.Web/Startup.cs
@@ -2,12 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PatientPortal.Web.Clients.PatientPortalApi;
+using PatientPortal.Web.Mappers;
 
 namespace PatientPortal.Web
 {
@@ -23,7 +27,30 @@ namespace PatientPortal.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews(opts =>
+            {
+                opts.Filters.Add(new AuthorizeFilter());
+            });
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(opts =>
+                {
+                    opts.Cookie.Name = "PatientPortal.Auth";
+                    opts.Cookie.HttpOnly = true;
+                    opts.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.None;
+                    opts.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Lax;
+                    opts.LoginPath = "/Home/Login";
+                    opts.LogoutPath = "/Home/Logout";
+                });
+
+            services.Configure<CookiePolicyOptions>(opts =>
+            {
+                opts.MinimumSameSitePolicy = Microsoft.AspNetCore.Http.SameSiteMode.Lax;
+                opts.HttpOnly = Microsoft.AspNetCore.CookiePolicy.HttpOnlyPolicy.Always;
+            });
+
+            services.AddTransient<IApiClient, ApiClient>();
+            services.AddTransient<IUserViewModelMapper, UserViewModelMapper>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,11 +66,12 @@ namespace PatientPortal.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseCookiePolicy();
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
