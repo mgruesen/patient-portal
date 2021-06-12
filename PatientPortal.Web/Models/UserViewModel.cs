1d40802af1c9a3f56dfb407b90b82343218f6dbe
Michael Gruesen <mgruesen7@gmail.com>
2021-06-12 01:08:30 +0000

    Refactor

--- PatientPortal.Web/Models/UserViewModel.cs
+++ PatientPortal.Web/Models/UserViewModel.cs
@@ -8,12 +8,16 @@ namespace PatientPortal.Web.Models
         public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string StreetAddress { get; set; }
+        public string StreetName { get; set; }
+        public string StreetNumber { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
         [EmailAddress]
         public string EmailAddress { get; set; }
         [Phone]
         public string PhoneNumber { get; set; }
-        public string IsMobilePhone { get; set; }
+        public bool IsMobilePhone { get; set; }
         public string ProviderName { get; set; }
         public string PolicyNumber { get; set; }
         public string GroupNumber { get; set; }
