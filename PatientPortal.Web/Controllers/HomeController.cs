77d368c98dee008df59ebfd665fecd4e28c66d89
Michael Gruesen <mgruesen7@gmail.com>
2021-06-12 00:36:43 +0000

    Add login/logout

--- PatientPortal.Web/Controllers/HomeController.cs
+++ PatientPortal.Web/Controllers/HomeController.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using PatientPortal.Web.Clients.PatientPortalApi;
 using PatientPortal.Web.Models;
 
 namespace PatientPortal.Web.Controllers
@@ -12,10 +20,14 @@ namespace PatientPortal.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IApiClient _apiClient;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IApiClient apiClient, IConfiguration configuration)
         {
             _logger = logger;
+            _apiClient = apiClient;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
@@ -32,6 +44,52 @@ namespace PatientPortal.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var user = await _apiClient.Login(model.Username, model.Password);
+
+            if (user == null)
+                return RedirectToAction("Login");
+
+            _configuration["ApiAuthHeader"] = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{model.Username}:{model.Password}"));
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+            };
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var authProperties = new AuthenticationProperties
+            {
+                IsPersistent = true,
+                ExpiresUtc = DateTimeOffset.Now.AddHours(1),//DateTime.UtcNow + DateTime.
+                RedirectUri = ""
+            };
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity), authProperties);
+
+            return RedirectToAction("Index", "User");
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
