a05c24f19301392ad56addfde7dcd91a5feefb3d
Michael Gruesen <mgruesen7@gmail.com>
2021-06-12 00:41:19 +0000

    Add login view model

---
+++ PatientPortal.Web/Models/LoginViewModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace PatientPortal.Web.Models
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
