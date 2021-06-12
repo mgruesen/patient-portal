9831570d55fb9b98fee4e217a5998974031bb156
Michael Gruesen <mgruesen7@gmail.com>
2021-06-12 01:08:42 +0000

    Refactor; add decomposition method

--- PatientPortal.Web/Mappers/UserViewModelMapper.cs
+++ PatientPortal.Web/Mappers/UserViewModelMapper.cs
@@ -5,30 +5,61 @@ namespace PatientPortal.Web.Mappers
 {
     public interface IUserViewModelMapper
     {
-        UserViewModel Map(UserModel user, PatientModel patient, ContactModel contact, ProviderModel provider);
+        UserViewModel Compose(UserModel user, PatientModel patient, ContactModel contact, ProviderModel provider);
+        (PatientModel, ContactModel, ProviderModel) Decompose(UserViewModel model);
     }
 
     public class UserViewModelMapper : IUserViewModelMapper
     {
-        public UserViewModel Map(UserModel user, PatientModel patient, ContactModel contact, ProviderModel provider)
+        public UserViewModel Compose(UserModel user, PatientModel patient, ContactModel contact, ProviderModel provider)
         {
-            var streetAddress = contact == null ? string.Empty
-                : $"{contact.StreetName} {contact.StreetNumber} {contact.City}, {contact.State} {contact.Zipcode}";
-            var isMobilePhone = contact == null ? string.Empty
-                : (contact.IsMobilePhone ? "Yes" : "No");
             return new UserViewModel
             {
                 Username = user.Username,
-                FirstName = patient.FirstName,
-                LastName = patient.LastName,
-                StreetAddress = streetAddress,
+                FirstName = patient?.FirstName ?? string.Empty,
+                LastName = patient?.LastName ?? string.Empty,
+                StreetName = contact?.StreetName ?? string.Empty,
+                StreetNumber = contact?.StreetNumber ?? string.Empty,
+                City = contact?.City ?? string.Empty,
+                State = contact?.State ?? string.Empty,
+                Zipcode = contact?.Zipcode ?? string.Empty,
                 EmailAddress = contact?.EmailAddress ?? string.Empty,
                 PhoneNumber = contact?.PhoneNumber ?? string.Empty,
-                IsMobilePhone = isMobilePhone,
+                IsMobilePhone = contact?.IsMobilePhone ?? false,
                 ProviderName = provider?.Name ?? string.Empty,
                 PolicyNumber = provider?.PolicyNumber ?? string.Empty,
                 GroupNumber = provider?.GroupNumber ?? string.Empty
             };
+        }
+
+        public (PatientModel, ContactModel, ProviderModel) Decompose(UserViewModel model)
+        {
+            var patientModel = new PatientModel
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName
+            };
+
+            var contactModel = new ContactModel
+            {
+                StreetName = model.StreetName,
+                StreetNumber = model.StreetNumber,
+                City = model.City,
+                State = model.State,
+                Zipcode = model.Zipcode,
+                EmailAddress = model.EmailAddress,
+                PhoneNumber = model.PhoneNumber,
+                IsMobilePhone = model.IsMobilePhone
+            };
+
+            var providerModel = new ProviderModel
+            {
+                Name = model.ProviderName,
+                PolicyNumber = model.PolicyNumber,
+                GroupNumber = model.GroupNumber
+            };
+
+            return (patientModel, contactModel, providerModel);
         }
     }
 }
